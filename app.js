9b4d66aa7a9a54afeb2d7457e7a1d02093442778
jersan <jeremysandau@gmail.com>
2018-05-24 17:42:32 -0600

    Added connection to mongoDB.  config.json file is required

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var config = require('./config.json');
+
+var mongoDB = 'mongodb://'+config.database.user+':'+config.database.password+'@'+config.database.host;
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 var app = express();
 
