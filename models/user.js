0461d1e62b99e2c1edfc8a7e9459930b58ee8e9e
jersan <jeremysandau@gmail.com>
2018-06-24 21:02:30 -0600

    Fixed issue - DB now uses 'user' collection in stead of 'users'

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true},
   password: { type: String, required: true}
-});
+}, { collection: 'user' });
 
 userSchema.plugin(uniqueValidator);
 
