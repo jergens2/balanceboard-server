2d7a26afac965a5b8bad7b4984456be09c6244e3
jersan <jeremysandau@gmail.com>
2018-06-28 14:22:57 -0600

    Added authentication via JsonWebToken

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require('../models/user');
 
+const secret = "1D78454C81ED9CB8E1348851F25DFE11BE0DCA6C277C5AC1CAB58B7B196B81C30F87F047F25871DAC35BA5ACA760EFF07F58A438FC2CDC1956EBC265E1";
 
 exports.register = function (req, res, next) {
     bcrypt.hash(req.body.password, 10)
@@ -25,6 +27,42 @@ exports.register = function (req, res, next) {
 };
 
 exports.authenticate = function (req, res, next) {
+    User.findOne({ 'email': req.body.email})
+        .then((user)=>{
+            if(!user){
+                return res.status(401).json({
+                    message: "Authorization failed.  Could not find an account with email '" + req.body.email + "'"
+                })
+            }
+            return bcrypt.compare(req.body.password, user.password)
+        })
+        .then((result) => {
+            if(!result){
+                return res.status(401).json({
+                    message: "Authentication failed.  Bad password."
+                })
+            }
+            const token = jwt.sign(
+                {
+                    email: user.email, 
+                    userId: user._id
+                },
+                secret,
+                {
+                    expiresIn: "1h"
+                }
+            );
+            res.status(200).json({
+                message: "Authentication successful.",
+                data: token
+            })
+
+         })
+         .catch((err) => {
+             return res.status(401).json({
+                 message: "Error.  Authentication failed."
+             })
+         })
 
 };
 
